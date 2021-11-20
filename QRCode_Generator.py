b73e440449846158e71c1dedf96086409d39a3db
Fromsii <78931701+Fromsii@users.noreply.github.com>
2021-11-20 15:19:08 +0100

    Update QRCode_Generator.py

--- QRCode_Generator.py
+++ QRCode_Generator.py
@@ -4,5 +4,5 @@ import qrcode
 link = input('Type your link:')
 img = qrcode.make(link)
 type(img)
-Savename = input('Type the name to save:')
+Savename = input('Name to save:')
 img.save(Savename + '.png')
